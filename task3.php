c268634d504c9b46a626bbd0fe539681794bac7b
Safayed Bin Islam <safayed.binislam@gmail.com>
2023-09-27 13:24:04 +0600

    Update task3.php

--- task3.php
+++ task3.php
@@ -1,1 +1,14 @@
+<?php
+//Task 3: Array Sorting  
 
+//Create an array called $grades with the following values: 85, 92, 78, 88, 95. Write a PHP function which takes "$grades" as an argument to sort the array in descending order and print the sorted grades as array.
+
+ $grades = [85, 92, 78, 88, 95];
+ 
+ function task3($array) : Array {
+     rsort($array);
+     print_r($array);
+     return $array;
+ }
+ 
+ task3($grades);
