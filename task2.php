fb2569050bd00134d88fac7b09e821a3004794c5
Safayed Bin Islam <safayed.binislam@gmail.com>
2023-09-27 13:03:35 +0600

    Update task2.php

--- task2.php
+++ task2.php
@@ -1,17 +1,23 @@
 <?php
- $numbers = array(range (1,10));
- 
- function task2(...$array){
-     
-     $i=0;
-     $len= strlen($array);
-     while($i<$len){
-		if ($array[$i]%2==0){
-        array_splice($array,$i,1);
-     	}
+
+//Task 2: Array Manipulation
+
+//Create an array called $numbers containing the numbers 1 to 10. Write a PHP function which takes the "$numbers" array as an argument to remove the even numbers from the array and print the resulting array.
+
+ $numbers = range(1, 10);
+print_r($numbers);
+
+function task2($array) {
+    $i = 0;
+    $len = count($array);
+    while ($i < $len) {
+        
+        if ( $array[$i] % 2 == 0) {
+            array_splice($array, $i, 1);
+        }
         $i++;
-     }
-     print_r($array);
-     
- }
- task2($numbers);
+    }
+    print_r($array);
+}
+
+task2($numbers);
