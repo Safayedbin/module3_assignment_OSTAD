eebc20d326eaaf3bc1c08ee1f29243eb7c210951
Safayed Bin Islam <safayed.binislam@gmail.com>
2023-09-27 22:16:58 +0600

    Update task5.php

--- task5.php
+++ task5.php
@@ -8,3 +8,17 @@ numbers, and special characters (!@#$%^&*()_+). Write a PHP program to generate 
 with a length of 12 characters using this function and print the password.
 */
 
+declare(strict_types=1);
+function generatePassword($length) : string{
+    $password = '';
+    for($i=0;$i<$length;$i++){
+    $c= mt_rand(33, 126);
+    $password .= chr($c);
+    }
+    echo $password;
+    return $password;
+}
+
+$sizeofpassword=12;
+echo "the Password generated is : ";
+$pass= generatePassword($sizeofpassword);
