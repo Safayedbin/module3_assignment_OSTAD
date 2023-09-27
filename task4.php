46869ebff7e1de3724466fd92ee462d7f95abe69
Safayed Bin Islam <safayed.binislam@gmail.com>
2023-09-27 22:00:13 +0600

    Update task4.php

--- task4.php
+++ task4.php
@@ -5,7 +5,7 @@
 //Each student has grades for three subjects: Math, English, and Science. Write a PHP function 
 //which takes "$studentGrades" as an argument to calculate and print the average grade for each student.
 
-
+declare(strict_types=1);
 $studentGrades=[
     [50,60,70],
     [80,90,85],
@@ -15,12 +15,15 @@ $studentGrades=[
     
 print_r($studentGrades);
 
-function AvgMark (...$mul){
-    $res;
-    for($i=0;$i<3;$i++;){
-        for($j=0;$j<3;$j++;){
-            $res +=$mul[$i][$j];
+function AvgMark (array $mul) : array{
+    $res=[0,0,0];
+    for($i=0;$i<3;$i++){
+        for($j=0;$j<3;$j++){
+            $res[$i]+=$mul[$i][$j];
         }
-        $res /= 3;
+        $res[$i] /= 3;
+        printf("The the average score of the student %d is %.2f\n", $i+1, $res[$i]);
     }
+    return $res;
 }
+AvgMark($studentGrades);
